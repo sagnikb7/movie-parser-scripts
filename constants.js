74001ca00081e498c524079f3040ed94baf49050
Sagnik <sagnikb7@gmail.com>
2021-04-09 02:45:09 +0530

    separate data dir

--- constants.js
+++ constants.js
@@ -6,9 +6,9 @@ const path = require('path');
 
 module.exports = {
     ALLOWED_FILE_TYPES    : ['.mp4', '.avi', '.mkv', '.flv', '.mov'],
-    DATABASE              : path.join(__dirname, 'movie.db'),
     MOVIE_MOVE_LIMIT      : 10,
-    MOVIE_LIST_FILENAME   : path.join(__dirname, 'movie_discovered.csv'),
+    DATABASE              : path.join(__dirname, 'data', 'movie.db'),
+    MOVIE_LIST_FILENAME   : path.join(__dirname, 'data', 'movie_discovered.csv'),
     MOVIE_DESTINATION_DIR : path.join(__dirname, 'MOVIE_FILES'),
     // MOVIE_DESTINATION_DIR : path.join('/mnt/HDD/', 'MOVIE_CHECK'),
 };
